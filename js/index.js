dd3971473763af4e74c37b1e71673ef1e5b9fa99
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-07-16 20:43:08 +0000

    Feat: Login sessions added.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
-$(() => {
-    $("#add").on("submit", function(event) {
-        event.preventDefault();
-        const serialNo = $("#serialNo").val();
-        const fullName = $("#fullName").val();
-        const degree = $("#degree").val();
-        const university = $("#university").val();
+const checkToken = () => {
+  const token = $.session.get("loggedInEmail");
 
-        console.log(serialNo, fullName, degree, university);
-    })
-})
+  if (!token) {
+    console.log("checked");
+    return (window.location.href = "./../index.html");
+  }
+};
+
+$(() => {
+  checkToken();
+
+  $("#add").on("submit", function (event) {
+    event.preventDefault();
+    const serialNo = $("#serialNo").val();
+    const fullName = $("#fullName").val();
+    const degree = $("#degree").val();
+    const university = $("#university").val();
+
+    console.log(serialNo, fullName, degree, university);
+  });
+
+  $("#logout").on("click", function (e) {
+    $.session.remove("loggedInEmail");
+    return (window.location.href = "./../index.html");
+  });
+});
